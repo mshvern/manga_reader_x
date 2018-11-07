104f379a41ea8c13a4c01725af0d1f5420da743d
mshvern <maxim.shvernenko@onix-systems.com>
2018-11-07 18:03:33 +0200

    working on it

--- reader/views.py
+++ reader/views.py
@@ -1,5 +1,27 @@
-from django.views.generic import TemplateView
+import os
+
+from django.views.generic import View
+from django.conf import settings
+from django.shortcuts import render
+
+manga_directory = 'reader/manga/'
+manga_path = os.path.join(settings.STATIC_ROOT, manga_directory)
 
 
-class MainView(TemplateView):
+def get_directory_as_nested_dict(path: str):
+    nest = {}
+    for element in os.listdir(path):
+        absolute_path = os.path.join(path, element)
+        static_relative_path = absolute_path[len(settings.STATIC_ROOT):]
+        if not os.path.isdir(absolute_path):
+            nest[static_relative_path] = None
+        else:
+            nest[static_relative_path] = get_directory_as_nested_dict(absolute_path)
+    return nest
+
+
+class MainView(View):
     template_name = "reader/index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name=self.template_name, context={"dirs": get_directory_as_nested_dict(manga_path)})
